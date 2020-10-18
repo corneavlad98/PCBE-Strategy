6b18ffeb80ec66cd4859142635bf302dbe5e9e5f
Alex Enache <alexandruenache98@gmail.com>
2020-10-18 14:49:46 +0300

    Added constants class

---
+++ src/main/java/MyConstants.java
@@ -0,0 +1,20 @@
+import java.util.Map;
+
+public class MyConstants {
+    public static final int TIME_TO_COLLECT_MEAT = 6;
+    public static final int AMOUNT_OF_MEAT = 4;
+
+    public static final int TIME_TO_COLLECT_WOOD = 10;
+    public static final int AMOUNT_OF_WOOD = 6;
+
+    public static final int TIME_TO_COLLECT_ROCK = 15;
+    public static final int AMOUNT_OF_ROCK = 15;
+
+    public static final int TIME_TO_COLLECT_GOLD = 20;
+    public static final int AMOUNT_OF_GOLD = 10;
+
+    public static final int TIME_TO_CONSTRUCT_HOUSE = 40;
+    public static final int TIME_TO_CONSTRUCT_CHURCH = 90;
+    public static final int TIME_TO_CONSTRUCT_SCHOOL = 60;
+
+}
