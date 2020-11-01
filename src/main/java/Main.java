b95811a26d4b36c20d3a705152b2c6e9ce5d9e1d
corneavlad98 <48709398+corneavlad98@users.noreply.github.com>
2020-11-01 15:16:47 +0200

    Renamed GUI to UserInterface. Added different color for players, space for another JPanel and "make rectange white" mouse click event.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,14 @@
 public class Main implements Runnable{
-    GUI gui = new GUI();
-    public static void main(String[] args){
-        new Thread(new Main()).start(); //player 1
-    }
 
-    @Override
-    public void run() {
-        while(true){
-            gui.repaint();
-        }
+    UserInterface ui = new UserInterface();
+    public static void main(String[] args){
+        new Thread(new Main()).start();
     }
+        @Override
+        public void run() {
+            while(true) {
+                ui.repaint();
+            }
+        }
+
 }
--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -5,33 +5,29 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.util.Random;
 
-public class GUI extends JFrame {
+public class UserInterface extends JFrame {
     int spacing = 2;
     Color myGreen = new Color(83, 205, 99); // Custom green color
-    Color myPurple = new Color(186, 12, 249); // Custom purple color
     Color myBrown = new Color(141, 69, 38); // Custom brown color
+    Color player1Color;
+    Color player2Color;
 
-    // initialise required arrays
-    int[][] wood = new int [32][32];
-    int[][] stone = new int [32][32];
-    int[][] gold = new int [32][32];
+    int[][] map = new int[32][32];
 
-    public int mx, my;
+    // initialise resource arrays
+    int[][] wood = new int[32][32];
+    int[][] stone = new int[32][32];
+    int[][] gold = new int[32][32];
+    boolean[][] clicked = new boolean[32][32];
 
-    // o sa trebuiasca sa folosim getter & setters cand lucram la partea de concurenta
-   /* public int getMx() {
-        return mx;
+    public int mouseX, mouseY;
+
+    public void generatePlayers(){
+        player1Color = Color.cyan;
+        player2Color= Color.pink;
+        map[0][31] = 1;
+        map[31][0] = 2;
     }
-    public void setMx(int mx) {
-        this.mx = mx;
-    }
-    public int getMy() {
-        return my;
-    }
-    public void setMy(int my) {
-        this.my = my;
-    }*/
-
     public void generateWood()
     {
         Random rand = new Random();
@@ -54,7 +50,7 @@ public class GUI extends JFrame {
         {
             for(int j = 6; j < 24; j++)
             {
-                if(rand.nextInt(100) < 20)
+                if(rand.nextInt(100) < 21)
                 {
                     stone[i][j] = 1;
                 }
@@ -69,7 +65,7 @@ public class GUI extends JFrame {
         {
             for(int j = 8; j < 22; j++)
             {
-                if(rand.nextInt(100) < 12)
+                if(rand.nextInt(100) < 14)
                 {
                     gold[i][j] = 1;
                 }
@@ -78,13 +74,18 @@ public class GUI extends JFrame {
         }
     }
 
-    public GUI(){
+
+    public UserInterface()
+    {
+        // set windows properties
         this.setTitle("GAME MAP");
-        this.setSize(665, 685);
+        //this.setSize(665, 685);
+        this.setSize(660, 785);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //when we click X button, application closes
         this.setVisible(true);
         this.setResizable(false);
 
+        generatePlayers();
         generateWood();
         generateStone();
         generateGold();
@@ -97,65 +98,84 @@ public class GUI extends JFrame {
 
         Click click = new Click();
         this.addMouseListener(click);
-    }
 
+    }
     public class Board extends JPanel{
         public void paintComponent(Graphics g){
             g.setColor(Color.DARK_GRAY);
-            g.fillRect(0,0,700,700);
+            //g.fillRect(0,0,700,700);
+            g.fillRect(0,0,700,800);
 
             for(int i = 0; i < 32; i++)
             {
                 for(int j = 0; j < 32; j++)
                 {
                     g.setColor(myGreen);
-                    if((i == 31 && j ==  0) ||( i == 0 && j == 31))  g.setColor(myPurple);
+                    if(map[i][j] == 1  && gold[i][j] == 0 && stone[i][j] == 0 && wood[i][j] == 0)
+                        g.setColor(player1Color);
+                    if(map[i][j] == 2  && gold[i][j] == 0 && stone[i][j] == 0 && wood[i][j] == 0)
+                        g.setColor(player2Color);
+
                     if(gold[i][j] == 0 && stone[i][j] == 0 && wood[i][j] == 1)
                         g.setColor(myBrown);
                     if(gold[i][j] == 0 && stone[i][j] == 1 && wood[i][j] == 0)
                         g.setColor(Color.gray);
                     if(gold[i][j] == 1 && stone[i][j] == 0 && wood[i][j] == 0)
                         g.setColor(Color.yellow);
-                    if((mx >= spacing+i*20 + 2) && (mx <= spacing+i*20-2*spacing+22) && (my >= spacing+j*20+20) && (my <= spacing+j*20+38))
+
+                    if(clicked[i][j])
+                        g.setColor(Color.WHITE);
+                    if((mouseX >= spacing+i*20 + 4) && (mouseX <= spacing+i*20-2*spacing+22) && (mouseY >= spacing+j*20+20) && (mouseY <= spacing+j*20+38))
                         g.setColor(Color.red);
+
                     g.fillRect(spacing+i*20, spacing+j*20 ,22-2*spacing, 22-2*spacing);
                 }
             }
         }
     }
 
-    public class Move implements MouseMotionListener{
+    public class Move implements MouseMotionListener {
         public void mouseDragged(MouseEvent e) {
 
         }
         public void mouseMoved(MouseEvent e) {
-            mx = e.getX();
-            my = e.getY();
+            mouseX = e.getX();
+            mouseY = e.getY();
         }
     }
-    public class Click implements MouseListener{
+
+    public class Click implements MouseListener {
         public void mouseClicked(MouseEvent e) {
-            if(inBoxX() != -1 && inBoxY() != -1){
-                System.out.println("Box that was clicked: ["+ inBoxX()+ "," + inBoxY() + "]");
+            if (inBoxX() != -1 && inBoxY() != -1) {
+                for (int i = 0; i < 32; i++) {
+                    for (int j = 0; j < 32; j++) {
+                        if (i == inBoxX() && j == inBoxY()) {
+                            clicked[i][j] = true;
+                            System.out.println("Box that was clicked: [" + inBoxX() + "," + inBoxY() + "]");
+                        } else clicked[i][j] = false;
+                    }
+                }
             }
         }
-        public void mousePressed(MouseEvent e) {
+        public void mousePressed (MouseEvent e){
 
         }
-        public void mouseReleased(MouseEvent e) {
+        public void mouseReleased (MouseEvent e){
 
         }
-        public void mouseEntered(MouseEvent e) {
+        public void mouseEntered (MouseEvent e){
 
         }
-        public void mouseExited(MouseEvent e) {
+        public void mouseExited (MouseEvent e){
 
         }
     }
+
+
     public int inBoxX(){
         for(int i = 0; i < 32; i++) {
             for(int j = 0; j < 32; j++) {
-                if((mx >= spacing+i*20 + 2) && (mx <= spacing+i*20-2*spacing+22) && (my >= spacing+j*20+20) && (my <= spacing+j*20+38))
+                if((mouseX >= spacing+i*20 + 2) && (mouseX <= spacing+i*20-2*spacing+22) && (mouseY >= spacing+j*20+20) && (mouseY <= spacing+j*20+38))
                     return i;
             }
         }
@@ -164,10 +184,11 @@ public class GUI extends JFrame {
     public int inBoxY(){
         for(int i = 0; i < 32; i++) {
             for(int j = 0; j < 32; j++) {
-                if((mx >= spacing+i*20 + 2) && (mx <= spacing+i*20-2*spacing+24) && (my >= spacing+j*20+20) && (my <= spacing+j*20+38))
+                if((mouseX >= spacing+i*20 + 2) && (mouseX <= spacing+i*20-2*spacing+24) && (mouseY >= spacing+j*20+20) && (mouseY <= spacing+j*20+38))
                     return j;
             }
         }
         return -1;
     }
+
 }
